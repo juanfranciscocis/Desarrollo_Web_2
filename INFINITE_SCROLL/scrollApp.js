a6cdc67f33e12cccf9316aec0d3c638e6c550077
Juan Francisco Cisneros <juanfrancistm2011@icloud.com>
2023-09-11 09:01:31 -0500

    infinite scroll

--- INFINITE_SCROLL/scrollApp.js
+++ INFINITE_SCROLL/scrollApp.js
@@ -20,8 +20,9 @@ function updateAPIURLWithNewCount(picCount) {
 
 //funciom para verificar carga de imagenes
 verificarImagenes = () =>{
-    totalImagenes++;
-    if (totalImagenes === totalImagenes){
+    imagenesCargadas++;
+    // Si true, ocultamos el spinner
+    if (imagenesCargadas === totalImagenes){
         listo = true;
         loader.hidden = true;
         count = 30;
@@ -33,7 +34,52 @@ async function getPhotos() {
     const resp = await fetch(apiUrl);
     fotos = await resp.json();
     console.log(fotos);
+    cargarFotos();
 }
+
+//funcion para configurar atributos en el DOM
+setAtrubutos = (elemento,atributos) =>{
+    for (const clave in atributos){
+        elemento.setAttribute(clave,atributos[clave]);
+    }
+}
+
+//funcion para mostrar fotos en el DOM
+cargarFotos = () => {
+    imagenesCargadas =0;
+    totalImagenes = fotos.length;
+    //agregar atributos a las imagenes
+    fotos.forEach((foto) => {
+        //crear link para la foto
+        const item = document.createElement("a");
+        setAtrubutos(item,{
+           href: foto.links.html, //link de la foto
+           target: "_blank", //abrir en una nueva pestaña
+        });
+        //crear imagen para la foto
+        const img = document.createElement("img");
+        setAtrubutos(img,{
+            src: foto.urls.regular, //link de la imagen
+            alt: foto.alt_description, //descripcion de la imagen
+            title: foto.alt_description, //titulo de la imagen
+        });
+        //evento para verificar carga de imagenes
+        img.addEventListener("load",verificarImagenes);
+        item.appendChild(img); //agregar imagen al link
+        contenedorImg.appendChild(item); //agregar link al contenedor
+    });
+}
+
+//funcion para cargar mas fotos
+window.addEventListener("scroll", () => {
+   //verificar si se llego al final de la pagina
+    console.log("scroll");
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && listo){
+        listo = false;
+        getPhotos();
+    }
+});
+
 
 getPhotos();
 
